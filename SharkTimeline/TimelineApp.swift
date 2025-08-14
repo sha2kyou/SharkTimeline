e82a44da8e6d18cccb4df3a389a36a3ed7f438db
SHARK YOU <41719224+sha2kyou@users.noreply.github.com>
2025-08-14 21:49:24 +0800

    Refactor: Rename TimelineApp to SharkTimeline
    Renamed the main application directory and updated all relevant
    project files and source code to reflect the new name.

---
+++ SharkTimeline/TimelineApp.swift
@@ -0,0 +1,15 @@
+// SharkTimeline.swift
+import SwiftUI
+
+@main
+struct SharkTimeline: App {
+    // 注册 AppDelegate
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        // 我们自己管理窗口，所以这里可以留空或使用一个隐藏的窗口
+        Settings {
+            SettingsView()
+        }
+    }
+}
