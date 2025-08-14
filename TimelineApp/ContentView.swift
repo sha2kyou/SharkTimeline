dc106f36c401bfef1e31d513a32d3664f5f3269f
SHARK YOU <41719224+sha2kyou@users.noreply.github.com>
2025-08-14 17:33:59 +0800

    Initial Commit

---
+++ TimelineApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimelineApp
+//
+//  Created by 李慕白 on 2025/8/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimelineAppTests/TimelineAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineAppTests.swift
+//  TimelineAppTests
+//
+//  Created by 李慕白 on 2025/8/14.
+//
+
+import Testing
+@testable import TimelineApp
+
+struct TimelineAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
